fae08b5610a5d83c2688d82bb14bbf25199cc1ba
Piyush Rajesh Deshkar <voucher77777@gmail.com>
2026-02-04 06:57:54 +0530

    Remove Firebase config details for security
    Removed sensitive Firebase configuration details.

--- firebase.ts
+++ firebase.ts
@@ -4,15 +4,14 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_4_adS0YQs8bGMbEvNSpLpW3BpCdvIAU",
-  authDomain: "mark1-7ce7e.firebaseapp.com",
-  databaseURL: "https://mark1-7ce7e-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "mark1-7ce7e",
-  storageBucket: "mark1-7ce7e.firebasestorage.app",
-  messagingSenderId: "147908886392",
-  appId: "1:147908886392:web:fef85a974d50189472128d",
-  measurementId: "G-D8TH2FNZJR"
-};
+  apiKey: "
+    ",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
